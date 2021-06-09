f4cb601154a6cbad1de10b722ffe52ec6e62c84c
Documenter.jl <documenter@juliadocs.github.io>
2021-06-09 21:44:26 +0000

    build based on 7e56f1e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Stacks.jl","page":"Stacks.jl","title":"Stacks.jl","text":"","category":"section"},{"location":"","page":"Stacks.jl","title":"Stacks.jl","text":"Documentation for Stacks.jl","category":"page"}]
+}
