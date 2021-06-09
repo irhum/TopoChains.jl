62c750f49b869d44cdc0d732ec043072fa7be862
Irhum Shafkat <13271155+irhum@users.noreply.github.com>
2021-06-09 04:23:55 +0000

    only leaving the Stacks code

--- docs/make.jl
+++ docs/make.jl
@@ -1,27 +1,11 @@
-using Documenter, Transformers
+using Documenter, Stacks
 
-using Transformers.Basic
-using Transformers.Basic: MultiheadAttention
-using Transformers.Pretrain
-using Transformers.Stacks
-using Transformers.GenerativePreTrain
-using Transformers.BidirectionalEncoder
-
-makedocs(sitename="Transformers.jl",
+makedocs(sitename="Stacks.jl",
          pages = Any[
-           "Home" => "index.md",
-           "Tutorial" => "tutorial.md",
-           "Basic" => "basic.md",
-           "Stacks" => "stacks.md",
-           "Pretrain" => "pretrain.md",
-           "Models" => [
-             "GPT" => "gpt.md",
-             "BERT" => "bert.md",
-           ],
-           "Datasets" => "datasets.md"
+           "Home" => "stacks.md",
          ],
          )
 
 deploydocs(
-    repo = "github.com/chengchingwen/Transformers.jl.git",
+    repo = "github.com/irhum/Stacks.jl.git",
 )
