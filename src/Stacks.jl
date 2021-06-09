c3f1568a710d32b87e8aa2b1bb648e0d546dc58a
Irhum Shafkat <13271155+irhum@users.noreply.github.com>
2021-06-09 04:42:56 +0000

    updating Project.toml

--- src/Stacks.jl
+++ src/Stacks.jl
@@ -3,8 +3,8 @@ module Stacks
 export NNTopo, @nntopo_str, @nntopo
 export Stack, show_stackfunc, stack
 
-include("./code.jl")
-include("./topology.jl")
-include("./stack.jl")
+include("code.jl")
+include("topology.jl")
+include("stack.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,38 +1,12 @@
-using Transformers
-using Transformers.Basic
-using Test
-using Random
-
-import Flux
-using Flux: gradient
+using Stacks
 
 const tests = [
-    "transformer",
     "nntopo",
-    "embed",
-    "basic",
-    "gpt",
-    "bert",
 ]
 
 Random.seed!(0)
 
-if v"1.0.0" <= VERSION  < v"1.4.0"
-    @test isempty(detect_ambiguities(Transformers, Transformers.GenerativePreTrain, Transformers.Basic, Transformers.Datasets))
-end
-
-if haskey(ENV, "TEST_TRANSFORMERS_PRETRAIN")
-    push!(tests, "pretrain")
-end
-
-@testset "Transformers" begin
-  if Flux.use_cuda[]
-    @info "Test CUDA"
-    include("test_cuda.jl")
-  else
-    @warn "CUDA unavailable, not testing GPU support"
-  end
-
+@testset "Stacks" begin
   for t in tests
     fp = joinpath(dirname(@__FILE__), "test_$t.jl")
     @info "Test $(uppercase(t))"
