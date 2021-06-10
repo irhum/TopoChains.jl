205e1bc8ea0afd3019dd9bd8d21f55131ed1f551
Irhum Shafkat <13271155+irhum@users.noreply.github.com>
2021-06-10 07:03:38 +0000

    Speeding load by removing explicit dependency on Flux

--- src/Stacks.jl
+++ src/Stacks.jl
@@ -1,10 +1,17 @@
 module Stacks
 
+using Requires
+
 export NNTopo, @nntopo_str, @nntopo, print_topo
-export Stack, show_stackfunc, stack
+export Stack, show_stackfunc
 
 include("code.jl")
 include("topology.jl")
 include("stack.jl")
 
+function __init__()
+    @require Flux = "587475ba-b771-5e3f-ad9e-33799f191a9c" begin
+        Flux.functor(s::Stack) = s.models, m -> Stack(s.topo, m...)
+    end
+end
 end
